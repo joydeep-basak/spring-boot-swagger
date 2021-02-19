4eb3c7c836167cf23acead038d98a361272de23f
joydeep <j_basak1981@yahoo.co.in>
2021-02-19 10:02:22 +0530

    project setup

---
+++ src/main/java/com/example/springbootswagger2/SpringBootSwagger2Application.java
@@ -0,0 +1,16 @@
+package com.example.springbootswagger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+//@EnableDiscoveryClient
+@SpringBootApplication
+@Configuration
+public class SpringBootSwagger2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSwagger2Application.class, args);
+	}
+}
